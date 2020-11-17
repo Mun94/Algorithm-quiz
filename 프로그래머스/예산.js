02aff1486d1f714cf4eedf92464bb454053fd170
Mun94 <seongwoo720@gmail.com>
2020-11-17 15:40:24 +0900

    done

---
+++ 프로그래머스/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget){
+    let result = 0;
+
+    return Math.max.apply(null,  d.sort((a,b) => a-b).map((m,index) => {
+            result += m
+            if(result <= budget){
+                return index+1
+            }else{
+                return 0
+            }
+    }).filter(a => a !== undefined))
+}
+
+console.log(solution([1,10,9,3,2,2,1,4,2,3], 10));
