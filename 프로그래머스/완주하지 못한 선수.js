1fa498c202accccb1d9dbfd97d64e12a2c626a72
Mun94 <seongwoo720@gmail.com>
2020-11-19 19:42:49 +0900

    done

--- 프로그래머스/완주하지 못한 선수.js
+++ 프로그래머스/완주하지 못한 선수.js
@@ -12,3 +12,13 @@ const p=['mislav', 'stanko', 'mislav'];
 const c=['stanko', 'ana', 'mislav'];
 
 console.log(solution(p,c));
+
+//////// 2번 방법
+
+function solution2(p,c){
+    p.sort();
+    c.sort();
+    return p.filter((v, idx) => v !== c[idx])[0]
+}
+
+console.log(solution2(['mislav', 'stanko', 'mislav'], ['stanko', 'ana', 'mislav']))
