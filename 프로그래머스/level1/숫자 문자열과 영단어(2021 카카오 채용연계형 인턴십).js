4c7d141d02d2d3ef46c25c7debc2f6a9bb72cfaa
Mun94 <seongwoo720@gmail.com>
2022-02-07 19:46:37 +0900

    done

--- 프로그래머스/level1/숫자 문자열과 영단어(2021 카카오 채용연계형 인턴십).js
+++ 프로그래머스/level1/숫자 문자열과 영단어(2021 카카오 채용연계형 인턴십).js
@@ -10,16 +10,16 @@ const solution = (s) => {
         'seven',
         'eight',
         'nine'
-    ]
+    ];
 
     for(let ix = 0; ix < num.length; ix++) {
         if(s.includes(num[ix])) {
             s = s.replace(num[ix], ix);
             ix = 0;
-        }
-    }
+        };
+    };
 
     return Number(s);
-}
+};
 
-console.log(solution("one4seveneightone"))
+console.log(solution("one4seveneightone"));
