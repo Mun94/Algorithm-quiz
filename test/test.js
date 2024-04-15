694d3e6ebe2f2b552dff45ecce6b2636b0f19035
Mun Seong Woo <seongwoo720@gmail.com>
2024-04-15 11:55:11 +0900

    init

--- test/test.js
+++ test/test.js
@@ -1,58 +1,30 @@
-/*
-남자 m 여자 n 만큼 있을때 3명당 한 팀인 조를 이루려고한다. 남자와 여자가 골고루 섞여서 몇 개의 조를 만들 수 있는지
-(남자만 있고 여자만 있는 조는 만들 수 없다.)
-*/
-
-// const men = ['m1', 'm2', 'm3'];
-// const women = ['w1', 'w2', 'w3'];
-
-// const array_reset = (menOrWomen,second,third) => {
-//     for(let i=0; i<third; i++){
-//         let a = second === true ? men[Math.floor(Math.random()*men.length)] : women[Math.floor(Math.random()*women.length)]
-//         menOrWomen.push(a);
-//         if(menOrWomen.filter(b => b===a).length > 1){
-//             menOrWomen.splice(i,third)
-//             i=0;
-//         }
-//     }
-// }
-
-// const m = 3;
-// const w = 2;
-
-// function solution(m,w){
-//     const men_array = [];
-//     const women_array = [];
-
-//    array_reset(men_array,true,m);
-//    array_reset(women_array,'',w);
-
-//    const new_array = new Array(men_array.join(), women_array.join());
-
-//     return new_array
-// }
-
-// console.log(solution(m,w)) <--- 너무 어렵게 생각하고 풀이 했던것 같음
-
-const m = 9;
-const w = 3;
-
-function solution(m, w) {
-  let men = 0;
-  let women = 0;
-  for (let i = 1; i < m; i++) {
-    men += i;
-  }
-
-  men = men * w;
-
-  for (let i = 1; i < w; i++) {
-    women += i;
-  }
-
-  women = women * m;
-
-  return men + women;
+function getApple(){
+  return new Promise( (resolve, reject) => {
+    setTimeout(() => resolve("apple"), 1000);
+  })
 }
 
-console.log(solution(m, w));
+function getBanana(){
+  return new Promise( (resolve, reject) => {
+    setTimeout(() => resolve("banana"), 2000);
+  })
+}
+
+async function getFruites(){
+
+  console.time()
+  let getApplePromise = getApple(); // async함수를 미리 논블록킹으로 실행한다. 
+  let getBananaPromise = getBanana(); // async함수를 미리 논블록킹으로 실행한다. 
+
+  // 이렇게 하면 각각 백단에서 독립적으로 거의 동시에 실행되게 된다.
+  console.log(getApplePromise)
+  console.log(getBananaPromise)
+
+  let a = await getApplePromise; // 위에서 받은 프로미스객체 결과 변수를 await을 통해 꺼낸다.
+  let b = await getBananaPromise; // 위에서 받은 프로미스객체 결과 변수를 await을 통해 꺼낸다.
+
+  console.log(`${a} and ${b}`); // 본래라면 1초+1초 를 기다려야 하는데, 위에서 1초기다리는 함수를 바로 연속으로 비동기로 불려왔기 때문에, 대충 1.01초만 기다리면 처리된다.
+  console.timeEnd()
+}
+
+getFruites();
