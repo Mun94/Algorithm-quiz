826af95c324f57cbe358ab0d608aff76f18fba3a
Mun Seong Woo <seongwoo720@gmail.com>
2025-01-21 08:38:11 +0900

    [Baekjoon] 11047 - 동전 0

---
+++ 백준/[11047] 동전 0.js
@@ -0,0 +1,29 @@
+const item = `10 4200
+1
+5
+10
+50
+100
+500
+1000
+5000
+10000
+50000`
+const input = item.toString().trim().split('\n');
+
+let [n, k] = input[0].split(' ').map(Number);
+
+let count = 0;
+
+for (let i = n; i > 0; i--) {
+  if (!k) {
+    break;
+  }
+
+  const m = Number(input[i]);
+
+  count += parseInt(k / m);
+  k = k % m;
+}
+
+console.log(count);
